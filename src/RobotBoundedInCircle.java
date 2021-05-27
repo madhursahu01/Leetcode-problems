a5e654940af7ccc2b1e8c5c4dd787ce7c4e7faae
madsahu <madhursahu@Madhurs-MacBook-Pro.local>
2021-05-27 20:52:50 +0530

    Robot bounded in circle Solution

---
+++ src/RobotBoundedInCircle.java
@@ -0,0 +1,53 @@
+
+/*
+On an infinite plane, a robot initially stands at (0, 0) and faces north.
+The robot can receive one of three instructions:
+
+"G": go straight 1 unit;
+"L": turn 90 degrees to the left;
+"R": turn 90 degrees to the right.
+The robot performs the instructions given in order, and repeats them forever.
+
+Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
+Example 1:
+
+Input: instructions = "GGLLGG"
+Output: true
+Explanation: The robot moves from (0,0) to (0,2), turns 180 degrees, and then returns to (0,0).
+When repeating these instructions, the robot remains in the circle of radius 2 centered at the origin.
+
+ */
+
+public class RobotBoundedInCircle {
+    public boolean isRobotBounded(String instructions) {
+
+        int[][] directions = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+        int x = 0;
+        int y = 0;
+        int direction = 0;
+        for(Character c : instructions.toCharArray())
+        {
+            if(c=='L')
+            {
+                direction = (direction+3) % 4;
+            }
+            else if(c == 'R')
+            {
+                direction = (direction+1) % 4;
+            }
+            else
+            {
+                x += directions[direction][0];
+                y += directions[direction][1];
+            }
+        }
+        return (x==0 && y==0) || direction!=0 ;
+    }
+
+    public static void main(String[] args)
+    {
+        RobotBoundedInCircle obj = new RobotBoundedInCircle();
+        String instruction = "GGLLGG";
+        System.out.println(obj.isRobotBounded(instruction));
+    }
+}
