6a40cd1351b4093f949fe728e4bd878de6d35496
madsahu <madhursahu@PHIGGINS-T480.valkyrie.net>
2021-08-19 10:20:12 +0530

    The Maze solution pushed

---
+++ src/TheMaze.java
@@ -0,0 +1,57 @@
+import org.junit.Assert;
+
+/*
+https://leetcode.com/problems/the-maze/
+
+There is a ball in a maze with empty spaces (represented as 0) and walls (represented as 1). The ball can go through the empty spaces by rolling up, down,
+left or right, but it won't stop rolling until hitting a wall.
+When the ball stops, it could choose the next direction.
+Given the m x n maze, the ball's start position and the destination, where start = [startrow, startcol] and destination = [destinationrow, destinationcol],
+return true if the ball can stop at the destination, otherwise return false.
+You may assume that the borders of the maze are all walls (see examples).
+Example 1:
+Input: maze = [[0,0,1,0,0],[0,0,0,0,0],[0,0,0,1,0],[1,1,0,1,1],[0,0,0,0,0]], start = [0,4], destination = [4,4]
+Output: true
+Explanation: One possible way is : left -> down -> left -> down -> right -> down -> right.
+ */
+public class TheMaze {
+
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        return dfs(maze, start, destination, visited);
+    }
+    public boolean dfs(int[][] maze, int[] start, int[] destination, boolean[][] visited) {
+        if (visited[start[0]][start[1]])
+            return false;
+        if (start[0] == destination[0] && start[1] == destination[1])
+            return true;
+        visited[start[0]][start[1]] = true;
+        int r = start[1] + 1, l = start[1] - 1, u = start[0] - 1, d = start[0] + 1;
+        while (r < maze[0].length && maze[start[0]][r] == 0) // right
+            r++;
+        if (dfs(maze, new int[] {start[0], r - 1}, destination, visited))
+            return true;
+        while (l >= 0 && maze[start[0]][l] == 0) //left
+            l--;
+        if (dfs(maze, new int[] {start[0], l + 1}, destination, visited))
+            return true;
+        while (u >= 0 && maze[u][start[1]] == 0) //up
+            u--;
+        if (dfs(maze, new int[] {u + 1, start[1]}, destination, visited))
+            return true;
+        while (d < maze.length && maze[d][start[1]] == 0) //down
+            d++;
+        if (dfs(maze, new int[] {d - 1, start[1]}, destination, visited))
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+        TheMaze obj = new TheMaze();
+        int[][] maze = {{0,0,1,0,0},{0,0,0,0,0},{0,0,0,1,0},{1,1,0,1,1},{0,0,0,0,0}};
+        int[] start = {0,4};
+        int[] destination = {4,4};
+        Assert.assertTrue(obj.hasPath(maze,start,destination));
+    }
+}
